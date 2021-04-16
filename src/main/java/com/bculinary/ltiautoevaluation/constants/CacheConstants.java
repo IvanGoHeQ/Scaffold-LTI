54cba303b1ff978c94440851b1e56b780ac32a09
Ivan <igomez@entornosdeformacion.com>
2021-04-16 11:38:50 +0200

    V1

---
+++ src/main/java/com/bculinary/ltiautoevaluation/constants/CacheConstants.java
@@ -0,0 +1,6 @@
+package com.bculinary.ltiautoevaluation.constants;
+
+public class CacheConstants {
+    public static final String COURSE_AUTOEVALUATION = "COURSE_AUTOEVALUATION";
+    public static final String QUESTIONS_AUTOEVALUATION = "QUESTIONS_AUTOEVALUATION";
+}
---
+++ src/main/java/com/bculinary/ltiautoevaluation/constants/SessionConstants.java
@@ -0,0 +1,6 @@
+package com.bculinary.ltiautoevaluation.constants;
+
+public class SessionConstants {
+    public static final String SUBMISSION = "SUBMISSION";
+    public static final String ANSWERS = "ANSWERS";
+}
